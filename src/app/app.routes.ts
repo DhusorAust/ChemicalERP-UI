a4def44c9759cc405e3bf200c80e421fa6fc7d8b
DhusorAust <anamulhasandhusor@gmail.com>
2025-11-01 22:59:19 +0600

    c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Bank } from './setting/bank/bank';
  
 //  // src/app/app.routes.ts
-// import { Routes } from '@angular/router';
-// import { startGuard, authGuard, loginGuard } from './auth-guard';
+import { Routes } from '@angular/router';
+import { startGuard, authGuard, loginGuard } from './auth-guard';
 
-// export const routes: Routes = [
-//   // { path: '',    canActivate: [startGuard], loadComponent: () => import('./home/home').then(m => m.Home) },
-//   // { path: 'home',  canActivate: [authGuard],  loadComponent: () => import('./home/home').then(m => m.Home) },
-//   // { path: 'login', canActivate: [loginGuard], loadComponent: () => import('./login/login').then(m => m.Login) },
-//   // { path: 'setting/bank',canActivate: [authGuard],loadComponent: () => import('./setting/bank/bank').then(m => m.Bank)},
-//   // { path: '**', redirectTo: '' }
-//   { path: '**', redirectTo: 'setting/bank' }
-// ];
+export const routes: Routes = [
+  { path: '',    canActivate: [startGuard], loadComponent: () => import('./home/home').then(m => m.Home) },
+  { path: 'home',  canActivate: [authGuard],  loadComponent: () => import('./home/home').then(m => m.Home) },
+  { path: 'login', canActivate: [loginGuard], loadComponent: () => import('./login/login').then(m => m.Login) },
+  { path: 'setting/bank',loadComponent: () => import('./setting/bank/bank').then(m => m.Bank)},
+  {
+    path: 'store/chemicalStore/requisition',loadComponent: () =>import('./store/chemicalStore/requisition/requisition').then(m => m.RequisitionComponent)   // <-- class name from requisition.ts
+  },
+
+  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: 'setting/bank' }
+];
 
 
 // src/app/app.routes.ts
@@ -31,15 +35,15 @@ import { Bank } from './setting/bank/bank';
 
 
 
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'store/chemicalStore/requisition' },
-  {
-    path: 'store/chemicalStore/requisition',
-    loadComponent: () =>
-      import('./store/chemicalStore/requisition/requisition')
-        .then(m => m.RequisitionComponent)   // <-- class name from requisition.ts
-  },
-  { path: '**', redirectTo: 'store/chemicalStore/requisition' }
-];
+// export const routes: Routes = [
+//   { path: '', pathMatch: 'full', redirectTo: 'store/chemicalStore/requisition' },
+  // {
+  //   path: 'store/chemicalStore/requisition',
+  //   loadComponent: () =>
+  //     import('./store/chemicalStore/requisition/requisition')
+  //       .then(m => m.RequisitionComponent)   // <-- class name from requisition.ts
+  // },
+//   { path: '**', redirectTo: 'store/chemicalStore/requisition' }
+// ];
--- src/app/menu.service.ts
+++ src/app/menu.service.ts
@@ -3,11 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, catchError, map, of, tap } from 'rxjs';
 import { MenuRow, MenuNode } from './menu.types';
 import { rowsToTree } from './menu.mapper';
+import { environment } from './environments/environment';
 
-// If you use environments:
-// import { environment } from '../environments/environment';
-// const API = `${environment.apiBase}/api/login/GetMenuLoad`;
-const API = 'http://localhost:56172/api/login/GetMenuLoad';
+ const apiBase = environment.apiBase; 
+
+ const  API = `${apiBase}/api/login/GetMenuLoad`;
 
 @Injectable({ providedIn: 'root' })
 export class MenuService {
@@ -18,7 +18,7 @@ export class MenuService {
   load() {
     if (this.loaded) return;
     this.loaded = true;
-
+    debugger;
     this.http.get<MenuRow[]>(API, { withCredentials: true }).pipe(
       catchError(err => { console.error('Menu API error', err); return of<MenuRow[]>([]); }),
       map(rows => rowsToTree(rows)),
